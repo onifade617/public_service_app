486ef97c4c2347e775a243b424a821190935b93d
ONIFADE MONSURU OLANREWAJU <onifade617@gmail.com>
2024-09-24 09:45:43 +0100

    Create page3.py

---
+++ page3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 09:44:03 2024
+
+@author: Awarri User
+"""
+
+import streamlit as st
+
+def display_page():
+    st.title("Page 2")
+    st.write("This is the second page of the app.")
+    st.write("You can add different content here.")
